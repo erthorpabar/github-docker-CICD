1c7e85c7b81f2b74cd24f5a63ee3c486c4940bb8
ycc1519202717 <ycc1519202717@gmail.com>
2026-01-08 15:46:04 +0800

    xxx

--- server.py
+++ server.py
@@ -4,37 +4,6 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# ——————————加载环境变量——————————
-from dotenv import load_dotenv
-load_dotenv()
-
-from pydantic_settings import BaseSettings # 优先系统环境变量，然后是.env文件，最后是默认值
-class Settings(BaseSettings):
-    
-    # LLM 配置
-    LLM_URL: str = " "
-    LLM_API_KEY: str = " "
-    LLM_MODEL: str = " "
-
-    # comfyui 配置
-    COMFYUI_API_URL: str = " "
-
-    # 数据库配置
-    MYSQL1: str = " "
-    MYSQL2: str = " "
-    
-    class Config:
-        # 指定从.env文件加载环境变量
-        env_file = ".env" # 允许从.env文件加载配置
-        env_file_encoding = "utf-8" # 指定编码
-        extra = "allow" # 允许额外的没用到的配置
-        case_sensitive = True  # 环境变量大小写敏感
-
-# 创建Settings的实例
-# 在其他文件中，你可以通过导入settings来访问这些配置
-settings = Settings()
-
-
 # ——————————————创建app——————————————
 import uvicorn
 from fastapi import FastAPI
